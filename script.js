f428b638645637f4884c3531c1245e58575c15d6
gsivriu <87988271+gsivriu@users.noreply.github.com>
2023-01-27 16:48:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let wordArray = [];
+function storeWord(event) {
+    let word = document.getElementById("wordEnter").value;
+    if(word === ""){
+      alert("please enter a word!");
+    }else{
+      wordArray.push(word);
+      console.log(wordArray);
+    }
+    document.getElementById("wordEnter").value = "";
+  }
+function checkWord() {
+    let oldImg = imageContainer.getElementsByTagName("img")[0];
+    if(oldImg) {
+        imageContainer.removeChild(oldImg);
+    }
+    let newword = document.getElementById("wordCheck").value;
+    document.getElementById("wordCheck").value = "";
+    let match = 0;
+    message.innerHTML = ""; // Clear previous message
+    for (let i = 0; i < wordArray.length; i++) {
+        if (newword === wordArray[i]) {
+          ++match;
+        }
+      }
+      if (match > 0) {
+        message.innerHTML +='Word found in dictionary';
+        let correctImg = document.createElement("img");
+        correctImg.src = "ok.jpeg";
+        imageContainer.appendChild(correctImg);
+      } else {
+        message.innerHTML +='No word found';
+        let correctImg = document.createElement("img");
+        correctImg.src = "not ok.jpeg";
+        imageContainer.appendChild(correctImg);
+      }
+      document.getElementById("wordCheck").value = "";
+    }
